3e716ea3184a97176839f0ab3c1a673e3e243dae
kaorun <kaorun55@gmail.com>
2012-10-11 02:33:55 +0900

    カラーカメラの設定をやってみた

--- KinectSDKv16Sample/05-ColorCameraSettings/MainWindow.xaml.cs
+++ KinectSDKv16Sample/05-ColorCameraSettings/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Kinect;
 
 namespace _05_ColorCameraSettings
 {
@@ -20,9 +10,118 @@ namespace _05_ColorCameraSettings
     /// </summary>
     public partial class MainWindow : Window
     {
+        KinectSensor kinect;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            kinect = KinectSensor.KinectSensors
+                .FirstOrDefault( k => k.Status == KinectStatus.Connected );
+
+            foreach ( Property item in AutoExposureProperties.ItemsSource ) {
+                item.Type = PropertyType<ColorCameraSettings>( item.Name );
+                item.Value = GetProtertyValue( item.Name );
+                item.Max = GetProtertyValue( "Max" + item.Name );
+                item.Min = GetProtertyValue( "Min" + item.Name );
+            }
+
+            foreach ( Property item in AutoWhiteBalanceProperties.ItemsSource ) {
+                item.Type = PropertyType<ColorCameraSettings>( item.Name );
+                item.Value = GetProtertyValue( item.Name );
+                item.Max = GetProtertyValue( "Max" + item.Name );
+                item.Min = GetProtertyValue( "Min" + item.Name );
+            }
+        }
+
+        /// <summary>
+        /// プロパティの名前から、値を取得する
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        private string GetProtertyValue( string propertyName )
+        {
+            var value = PropertyInvoke( kinect.ColorStream.CameraSettings, propertyName );
+            return (value != null) ?  value.ToString() : "---";
+        }
+
+        /// <summary>
+        /// プロパティの名前の文字列から、プロパティの値を取得する
+        /// http://devlabo.blogspot.jp/2010/12/c.html
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        private object PropertyInvoke<T>( T instance, string propertyName )
+        {
+            var type = typeof( T );
+            var property = type.GetProperty( propertyName );
+            if ( property == null ) {
+                return null;
+            }
+
+            return property.GetValue( instance );
+        }
+
+        /// <summary>
+        /// プロパティの名前から、プロパティの型を取得する
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        private string PropertyType<T>( string propertyName )
+        {
+            var type = typeof( T );
+            var property = type.GetProperty( propertyName );
+            if ( property == null ) {
+                return null;
+            }
+
+            return property.PropertyType.ToString();
+        }
+    }
+
+    /// <summary>
+    /// 表示する項目
+    /// </summary>
+    public class Property
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        public string Max
+        {
+            get;
+            set;
+        }
+
+        public string Min
+        {
+            get;
+            set;
+        }
+
+
+        public string Detail
+        {
+            get;
+            set;
         }
     }
 }
